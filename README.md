9437edb60f93aed7ab50be12e77571b7510d713d
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-07-24 20:45:34 +0700

    chore: push src

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
-# Replugged plugin template
+<p>
+  <h1 align="center">Magnificent</h1>
+</p>
 
-See the [plugin development guide](https://guide.replugged.dev/docs/plugins/getting-started) for more information on using this template.
+<p align="center">
+  <a href="https://github.com/Socketlike/Magnificent/releases/latest">
+    <img alt="latest release" src="https://img.shields.io/github/v/release/Socketlike/Magnificent?label=version&sort=semver">
+  </a>
+  <a href="https://github.com/Socketlike/Magnificent/actions/workflows/lint.yml">
+    <img alt="lint status" src="https://img.shields.io/github/actions/workflow/status/Socketlike/Magnificent/lint.yml?label=lint">
+  </a>
+  <a href="https://github.com/Socketlike/SpotifyModal/actions/workflows/release.yml">
+    <img alt="build status" src="https://img.shields.io/github/actions/workflow/status/Socketlike/Magnificent/release.yml?label=build">
+  </a>
+</p>
+
+<p align="center">
+  <a href="https://replugged.dev/install?identifier=Socketlike/Magnificent&source=github">
+    <img alt="install" src="https://img.shields.io/github/v/release/Socketlike/Magnificent?label=Install&sort=semver&style=for-the-badge">
+  </a>
+</p>
+
+<p align="center">
+  a post-swc Replugged plugin that removes the width & height restriction on images' source URLs in popouts.
+</p>
+
+## But I see no difference!
+
+This only removes the `?width=(number)&height=(number)` at the end of images' source URLs.  
+The native CSS sizing is unmodified.
+
+## What is this for?
+
+[One specific theme](https://github.com/SiriusBYT/flashcord).  
+Or any theme really.
+
+## Previews?
+
+![insert very magnificent and trauma inducing gif for dark theme enjoyers](https://media.discordapp.net/attachments/907813363294277652/1133028405298864178/FdcFooiY.gif)
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,53 @@
-import { Injector, Logger, webpack } from "replugged";
+import { Injector, Logger, util, webpack } from 'replugged';
 
-const inject = new Injector();
-const logger = Logger.plugin("PluginTemplate");
+const injector = new Injector();
+const logger = Logger.plugin('Magnificent');
 
-export async function start(): Promise<void> {
-  const typingMod = await webpack.waitForModule<{
-    startTyping: (channelId: string) => void;
-  }>(webpack.filters.byProps("startTyping"));
-  const getChannelMod = await webpack.waitForModule<{
-    getChannel: (id: string) => {
-      name: string;
-    };
-  }>(webpack.filters.byProps("getChannel"));
+// we don't need to go in depth into typing the constructor; it's not needed
+interface ImageComponent {
+  (): void;
+  prototype: {
+    render: () => JSX.Element;
+  };
+}
 
-  if (typingMod && getChannelMod) {
-    inject.instead(typingMod, "startTyping", ([channel]) => {
-      const channelObj = getChannelMod.getChannel(channel);
-      logger.log(`Typing prevented! Channel: #${channelObj?.name ?? "unknown"} (${channel}).`);
+export const start = async (): Promise<void> => {
+  const imageModule = await webpack.waitForModule<Record<string, ImageComponent>>(
+    webpack.filters.bySource('"imageClassName"'),
+  );
+
+  const imageComponent = imageModule
+    ? webpack.getFunctionBySource<ImageComponent>(imageModule, 'Ref')
+    : undefined;
+
+  if (imageComponent && 'prototype' in imageComponent)
+    injector.after(imageComponent.prototype, 'render', (_, res: JSX.Element): JSX.Element => {
+      // this will only let "popout" images pass, because "popout" images are not clickable
+      // hence no [class*=clickable-]
+      if (!res?.props?.children?.props?.className?.match?.(/clickable-/)) {
+        // this function's typing is ridiculous
+        // it's designed *for* JSX.Element trees, yet **doesn't allow** JSX.Element?
+        // time for typecast fuckery :husk:
+        const image = util.findInReactTree(res as unknown as util.Tree, (_): boolean => {
+          const tree = _ as unknown as JSX.Element;
+          // only let images without className pass - the "popout" images doesn't have className
+          return tree?.type === 'img' && tree.props && !tree.props.className;
+        }) as unknown as JSX.Element;
+
+        if (image && image.props?.src === 'string')
+          image.props.src = image.props.src.replace(/\?width=[0-9]+&height=[0-9]+$/, '');
+      }
+
+      return res;
     });
-  }
-}
+  else
+    logger.error(
+      !imageComponent
+        ? 'Unable to get image component'
+        : 'Expected image component but got something else',
+      imageModule,
+      imageComponent,
+    );
+};
 
-export function stop(): void {
-  inject.uninjectAll();
-}
+export const stop = (): void => injector.uninjectAll();
