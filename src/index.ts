a823bf38751905a67a95723d70708426eb7b8dac
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-10-16 15:24:45 +0700

    fix: module querying and handling

--- src/index.ts
+++ src/index.ts
@@ -12,33 +12,34 @@ interface ImageComponent {
 }
 
 export const start = async (): Promise<void> => {
-  const imageModule = await webpack.waitForModule<Record<string, ImageComponent>>(
+  const imageComponent = await webpack.waitForModule<ImageComponent>(
     webpack.filters.bySource('"imageClassName"'),
   );
 
-  const imageComponent = imageModule
-    ? webpack.getFunctionBySource<ImageComponent>(imageModule, 'Ref')
-    : undefined;
-
-  if (imageComponent && 'prototype' in imageComponent)
+  if (imageComponent?.prototype)
     injector.after(imageComponent.prototype, 'render', (_, res: JSX.Element): JSX.Element => {
-      // this will only let "popout" images pass, because "popout" images are not clickable
-      // hence no [class*=clickable-]
-      if (!res?.props?.children?.props?.className?.match?.(/clickable-/)) {
-        // this function's typing is ridiculous
-        // it's designed *for* JSX.Element trees, yet **doesn't allow** JSX.Element?
-        // time for typecast fuckery :husk:
-        const image = util.findInReactTree(res as unknown as util.Tree, (_): boolean => {
-          const tree = _ as unknown as JSX.Element;
-          // only let images without className pass - the "popout" images doesn't have className
-          return tree?.type === 'img' && tree.props && !tree.props.className;
-        }) as unknown as JSX.Element;
+      // fail fast if image component is rendered as an embed
+      let isEmbed = false;
 
-        if (image && typeof image.props?.src === 'string')
-          image.props.src = image.props.src
-            .replace(/\?width=[0-9]+&height=[0-9]+$/, '')
-            .replace('media.discordapp.net', 'cdn.discordapp.com');
-      }
+      // please fix the typings of this method it's truly ridiculous
+      const image = util.findInReactTree(res as unknown as util.Tree, (_): boolean => {
+        const element = _ as unknown as JSX.Element;
+
+        if (element?.props?.className?.match?.(/clickable-/))
+          isEmbed = true;
+
+        return (
+          element?.type === 'img' &&
+          typeof element?.props?.src === 'string' &&
+          !element?.props?.className ||
+          isEmbed
+        );
+      }) as unknown as JSX.Element;
+
+      if (image && !isEmbed)
+        image.props.src = image.props.src
+          .replace(/\?width=[0-9]+&height=[0-9]+$/, '')
+          .replace('media.discordapp.net', 'cdn.discordapp.com');
 
       return res;
     });
@@ -47,7 +48,6 @@ export const start = async (): Promise<void> => {
       !imageComponent
         ? 'Unable to get image component'
         : 'Expected image component but got something else',
-      imageModule,
       imageComponent,
     );
 };
